d3018e09cab0b100722f3cc30e724f55721c5267
RAPHAELA DE CASTRO RINCON WAGNER <raphaela.crwagner@senacsp.edu.br>
2019-10-31 11:23:43 -0300

    Models incompleto

---
+++ src/model/Fornecedor.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author raphaela.crwagner
+ */
+public class Fornecedor {
+    private int id;
+    private String nome;
+    private char status;
+
+    public Fornecedor(int id, String nome, char status) {
+        this.id = id;
+        this.nome = nome;
+        this.status = status;
+    }
+
+    public Fornecedor() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public char getStatus() {
+        return status;
+    }
+
+    public void setStatus(char status) {
+        this.status = status;
+    }
+    
+    
+}
---
+++ src/model/Titulo.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author raphaela.crwagner
+ */
+public class Titulo {
+    private int id;
+    private String nome;
+
+    public Titulo(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Titulo() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
+}
