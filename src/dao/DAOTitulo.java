bebe72a0087f007f47aa7599963f615b0638baa2
MATHEUS MAKOTO KODAMA <matheus.mkodama@senacsp.edu.br>
2019-11-04 16:06:55 -0300

    DAO TI/VE

--- src/dao/DAOTitulo.java
+++ src/dao/DAOTitulo.java
@@ -1,14 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dao;
 
-/**
- *
- * @author raphaela.crwagner
- */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import lojademanga.ConnectionFactory;
+import model.Titulo;
 public class DAOTitulo {
+    private Connection conn;
     
+    public DAOTitulo(){
+        
+        conn = ConnectionFactory.getConnection();
+    }
+public ArrayList<Titulo> selectAll() {
+        String sql = "Select * from Produto";
+
+        try {
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            ArrayList<Titulo> list = new ArrayList<>();
+
+            while (rs.next()) {
+                Titulo ti = new Titulo();
+                ti.setId(rs.getInt("ti_id"));
+                ti.setNome("ti_nome");
+
+                list.add(ti);
+            }
+            ConnectionFactory.closeConnection(conn, stmt, rs);
+            return list;
+        } catch (SQLException ex) {
+            System.err.println("DAO PRODUTO: " + ex);
+            return null;
+        }
+
+    }
+
 }
--- src/dao/DAOVenda.java
+++ src/dao/DAOVenda.java
@@ -1,14 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dao;
 
-/**
- *
- * @author raphaela.crwagner
- */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import lojademanga.ConnectionFactory;
+import model.Venda;
+
 public class DAOVenda {
+    private Connection conn;
     
+    public DAOVenda(){
+        
+        conn = ConnectionFactory.getConnection();
+    }
+public ArrayList<Venda> selectAll() {
+        String sql = "Select * from Produto";
+
+        try {
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            ArrayList<Venda> list = new ArrayList<>();
+
+            while (rs.next()) {
+                Venda ve = new Venda();
+                ve.setId(rs.getInt("ve_id"));
+                ve.setFuncionarios(rs.getInt("ve_funcionario"));
+                ve.setCliente(rs.getInt("ve_cliente"));
+                
+
+                list.add(ve);
+            }
+            ConnectionFactory.closeConnection(conn, stmt, rs);
+            return list;
+        } catch (SQLException ex) {
+            System.err.println("DAO PRODUTO: " + ex);
+            return null;
+        }
+
+    }
+
 }
