af5c61baa5d1e77a4f61cbcb3520c5506cff8bbf
MATHEUS MAKOTO KODAMA <matheus.mkodama@senacsp.edu.br>
2019-11-07 08:40:48 -0300

    BD Atualizado

--- Documentos/BD-MangaStore.sql
+++ Documentos/BD-MangaStore.sql
@@ -1,4 +1,3 @@
-
 create database MangaStore;
 use MangaStore;
 
@@ -32,11 +31,11 @@ ti_id int auto_increment not null,
 ti_nome varchar(45)not null unique,
 primary key (ti_id)
 );
-create table estilo (
-et_id smallint not null auto_increment,
-et_nome varchar(45)not null,
-primary key (et_id)
-);
+#create table estilo (
+#et_id smallint not null auto_increment,
+#et_nome varchar(45)not null,
+#primary key (et_id)
+#);
 
 create table autor(
 au_id smallint not null auto_increment,
@@ -51,11 +50,11 @@ for_status char not null,
 primary key (for_id)
 );
 
-create table genero(
-gen_id tinyint not null auto_increment,
-gen_nome varchar(45)not null unique,
-primary key (gen_id)
-);
+#create table genero(
+#gen_id tinyint not null auto_increment,
+#gen_nome varchar(45)not null unique,
+#primary key (gen_id)
+#);
 
 create table prateleira(
 pt_id smallint not null auto_increment,
@@ -73,21 +72,23 @@ primary key (es_id)
 create table produto (
 pd_id int not null auto_increment unique,
 pd_preco float not null,
-pd_status char not null,
+#pd_status char not null,
 pd_titulo int not null,
 pd_autor smallint not null,
-pd_fornecedor smallint not null,
-pd_genero tinyint not null,
-pd_estante smallint not null,
-pd_prateleira smallint not null,
-pd_estilo smallint not null,
+#pd_fornecedor smallint not null,
+#pd_genero tinyint not null,
+#pd_estante smallint not null,
+#pd_prateleira smallint not null,
+#pd_estilo smallint not null,
+pd_quantidade int not null,
 foreign key(pd_titulo) references titulo(ti_id),
 foreign key(pd_autor) references autor(au_id),
-foreign key(pd_fornecedor) references fornecedor(for_id),
-foreign key(pd_genero) references genero(gen_id),
-foreign key (pd_estante) references estante(es_id),
-foreign key (pd_prateleira) references prateleira(pt_id),
-foreign key (pd_estilo) references estilo(et_id)
+#foreign key(pd_fornecedor) references fornecedor(for_id),
+#foreign key(pd_genero) references genero(gen_id),
+#foreign key (pd_estante) references estante(es_id),
+#foreign key (pd_prateleira) references prateleira(pt_id)
+#foreign key (pd_estilo) references estilo(et_id)
+primary key (pd_id)
 );
 
 create table venda (
@@ -120,7 +121,22 @@ foreign key (ep_prateleira) references prateleira(pt_id)
 insert into cliente (cli_cpf,cli_nome,cli_email,cli_cel,cli_tel,cli_cep,cli_endereco,cli_cidade,cli_bairro,cli_complemento,cli_status)
 values (12345678910,'Ana Maria Praga','am.praga@disgracamail.com','999999999','54785236','21445855','Rua globo 666','Sao Paulo','Jardim iluminati','','A');
 
+ insert into titulo (ti_nome)
+  values ('Bleach');
+  
+insert into autor (au_nome)
+	values ('Tite Kubo');
+  
+insert into produto (pd_preco,pd_titulo,pd_autor,pd_quantidade)
+ values (10.00,1,1,100);
+ 
 
-
+  
+insert into funcionarios(func_nome)
+ values('Rubens Toreto');
+ 
+#insert into fornecedor (for_nome,for_status)
+	#values ('Panini','A');
+ 
 
 
